cb07284286b0e24db54b11974dfbd6b308ab1c54
daniil225 <31102560+daniil225@users.noreply.github.com>
2024-05-09 19:08:23 +0200

    lab 5 start
    1) Разобрался с разделяемой памятью. Управлять процессами и давать через нее команды выглядит вполне естественно. При этом управление происходит с указанием конкретного процесса поэтому проблем с управлением не возникнет.
    2) Организовал управление процессом посредством сегмента разделяемой памяти.
    
    3) Общим ресурсом в моем случае является очередь. Поэтому ее следует защитить семафором

--- lab5/main.c
+++ lab5/main.c
@@ -4,24 +4,130 @@
 #include <stdlib.h>    // fprintf(), fscanf()
 #include <inttypes.h> 
 #include <sys/resource.h>
+#include <sys/wait.h>   // waitpid
+#include <sys/signal.h> // signal, kill
 
-#define MAX_COUNT_PID 5
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-pid_t Pids[MAX_COUNT_PID]; // Массив идентификаторов процесса 
+#define PID_POOL 100
+#define MAX_CURR_PID 3
+#define MSG_BUFFER_SIZE 256
+#define DELAY_TIME 1000 // Время сна перед очередной проверкой для процесса 
+#define SHM_KEY 1234
 
+pid_t pidPool[PID_POOL]; // Массив идентификаторов процесса 
+pid_t currPid[MAX_CURR_PID]; // Массив максимального количества одновременно запущенных программ
+
+/* Структура управления действиями процесса */
+typedef struct  _pidCommand
+{
+    int32_t type_action; // Тип действия 0 - ничего не делать, 1 - загрузить данные из очереди, 2 - загрузить данные в очередб 3 - завершить процесс
+    pid_t pid; // Процесс которому предназначено действие 
+    int32_t CanSend; //  Можно ли отправлять запрос 
+}PidCommand;
+
+PidCommand *pid_command;
+
+
+void alarm_handler(int sig){ } // Функция пустышка для пробуждения процесса 
 
 int main()
 {
 
-    if(setpriority(PRIO_PROCESS, getpid(), -1) < 0)
+    signal(SIGALRM, alarm_handler);
+    /* Создать сегмент разделаемой памяти */
+    int shm_id = shmget(SHM_KEY, sizeof(PidCommand), IPC_CREAT | 0666);
+    if(shm_id == -1)
     {
-        perror("setpriority");
+        perror("shmget");
+        exit(EXIT_FAILURE);
     }
 
+    // Присоединяем сегмент разделаемой памяти к своему адресному пространству 
+    void *shm_addr = shmat(shm_id, NULL, 0);
+    if(shm_addr == (void*)-1)
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+    pid_command = (PidCommand*)shm_addr;
 
-    int priority = getpriority(PRIO_PROCESS, getpid());
+    pid_t pid;
 
-    printf("Priority = %d\n", priority);
+    if((pid = fork()) == 0)
+    {
+        // Цикл процесса 
+        while(1)
+        {
+            if(pid_command->pid == getpid())
+            {
+                if(pid_command->type_action == 1)
+                {
+                    printf("Action 1\n");
+                    pid_command->CanSend = 1;
+                }
+                else if(pid_command->type_action == 2)
+                {
+                    printf("Action 2\n");
+                    pid_command->CanSend = 1;
+                }
+                else if(pid_command->type_action == 3)
+                {
+                    printf("Action 3\n");
+                    pid_command->CanSend = 1;
+                    break;
+                }
+            }
+            sleep(DELAY_TIME); // Сон, не поедаем процессорное время. Раз в 1000сек смотрим не изменилось ли состояние и не пора ли нам чего то сделать 
+        }
 
-    return 0;
+        printf("Child pid = %d\n", getpid());
+        execl("prog1", "", NULL);
+        perror("execl");
+    }
+    else  if(pid < 0)
+    {   
+        
+        perror("Fork error\n");
+        exit(-1);
+    }
+
+    pid_command->pid = pid;
+    pid_command->type_action = 1;
+    pid_command->CanSend = 0;
+
+    while(kill(pid, SIGALRM));
+    while(pid_command->CanSend == 0); // Пока ноль не идем далее    
+    
+    pid_command->type_action = 2;
+    pid_command->CanSend = 0;
+    
+
+    while(kill(pid, SIGALRM));
+    while(pid_command->CanSend == 0); // Пока ноль не идем далее    
+    
+    
+    pid_command->type_action = 3;
+    while(kill(pid, SIGALRM));
+    while(pid_command->CanSend == 0); // Пока ноль не идем далее    
+    
+
+    int status;
+    waitpid(pid, &status, 0);
+
+    // Отсоединить сегмент разделяемой памяти от своего адресного пространства
+    if (shmdt(shm_addr) == -1) {
+        perror("shmdt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Удалить сегмент разделяемой памяти
+    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+        perror("shmctl");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
